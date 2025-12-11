15e14ecf4c178391c285e7898231b7b8ca596bf2
adebankesobanke <adebankesobanke9@gmail.com>
2025-12-11 11:10:06 +0100

    Add CSV loader for raw patients table

--- Desktop/Projects/HospitalDB_ETL/phase1/sql/csv_loaders/load_raw_patients.sql
+++ Desktop/Projects/HospitalDB_ETL/phase1/sql/csv_loaders/load_raw_patients.sql
@@ -1,6 +1,10 @@
-TRUNCATE TABLE raw_patients;
-
-COPY raw_patients
-FROM '/path/to/patients.csv'
-DELIMITER ','
+COPY raw.patients_raw(
+    patient_id,
+    first_name,
+    last_name,
+    gender,
+    dob,
+    branch_id
+)
+FROM 'C:/Users/user/Desktop/Projects/HospitalDB_ETL/phase1/csv/patients.csv'
 CSV HEADER;
